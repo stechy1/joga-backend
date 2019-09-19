5128b0bd0126148fceba5a1c78b138fd41db986d
Petr Štechmüller <stechy12@gmail.com>
2019-09-19 18:41:12 +0200

    Aktualizace dumpu databáze.

--- database.dump.sql
+++ database.dump.sql
@@ -31,7 +31,7 @@ CREATE TABLE `carousel` (
   `view_order` int(11) NOT NULL DEFAULT '-1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `carousel` (
 
 LOCK TABLES `carousel` WRITE;
 /*!40000 ALTER TABLE `carousel` DISABLE KEYS */;
-INSERT INTO `carousel` VALUES (1,'test','sdfasfa','1ee114e53b78a8dac5826e46f59ce4f3b93c1565.png',1,1),(2,'rfgsdgd','dfsgdfgdf','bfc9cbeb5160e9df5e16b01d90f4988a4c7e27bd.png',1,0);
+INSERT INTO `carousel` VALUES (3,'Panorama','Toto je na lanzarote','e222133a8ccd0ebce750f92a2deb865cfa370c2c.jpg',1,0),(4,'Moře','Pohled na moře','3dd251987d81c0e798dd0f59502126bb35dc6160.jpg',1,1),(5,'Vodopád','Velký pěkný vodopád','fae6e90b04c889d09dca33d0bff8400715d0ca36.jpg',1,3),(6,'Zákusky','Jeden velký stál 5€, malý za 1.5€','8046571112385af24473533e325c268a55aca0c9.jpg',1,4),(7,'Sevilla','Zde se natáčely mimo jiné i hvězdné války','25743d88e90d9329a178f9b6d22d2f7a9f8c6e9b.jpg',1,2),(8,'Katedrála','Epický pohled','5f9115c8ca8f71ca9a5451763eb5f6d8777081b5.jpg',1,5);
 /*!40000 ALTER TABLE `carousel` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,9 +103,10 @@ DROP TABLE IF EXISTS `lecture_type`;
 CREATE TABLE `lecture_type` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `description` text COLLATE utf8_czech_ci,
   `price` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,33 +115,8 @@ CREATE TABLE `lecture_type` (
 
 LOCK TABLES `lecture_type` WRITE;
 /*!40000 ALTER TABLE `lecture_type` DISABLE KEYS */;
-INSERT INTO `lecture_type` VALUES (1,'děti',50),(2,'dospělí',150);
+INSERT INTO `lecture_type` VALUES (1,'děti','popis pro děti',50),(2,'dospělí','poipasfdf',150);
 /*!40000 ALTER TABLE `lecture_type` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `lecture_types`
---
-
-DROP TABLE IF EXISTS `lecture_types`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lecture_types` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
-  `price` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `lecture_types`
---
-
-LOCK TABLES `lecture_types` WRITE;
-/*!40000 ALTER TABLE `lecture_types` DISABLE KEYS */;
-INSERT INTO `lecture_types` VALUES (1,'děti',150);
-/*!40000 ALTER TABLE `lecture_types` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -162,7 +138,7 @@ CREATE TABLE `lectures` (
   PRIMARY KEY (`id`),
   KEY `fk_trainer` (`trainer`),
   KEY `fk_type` (`type`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -171,6 +147,7 @@ CREATE TABLE `lectures` (
 
 LOCK TABLES `lectures` WRITE;
 /*!40000 ALTER TABLE `lectures` DISABLE KEYS */;
+INSERT INTO `lectures` VALUES (2,2,1,1567317600,1567320300,10,'dsfasgvsfv',1),(3,2,2,1567320600,1567323300,15,'dfgsdgdf',1),(4,2,1,1567494900,1567499400,7,'gsdgdfbdsfgd',1),(5,2,2,1568186400,1568189100,15,'gsdgsdhbsbhgf',1),(6,2,1,1568207400,1568210400,4,'gdfsgdgfg',0),(8,2,1,1567750200,1567752600,10,'gdsfgbdsgbdf',1),(9,2,1,1567767600,1567770300,10,'gbdshbfdjgjmgh',1);
 /*!40000 ALTER TABLE `lectures` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -186,10 +163,17 @@ CREATE TABLE `users` (
   `email` varchar(255) COLLATE utf8_czech_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_czech_ci NOT NULL,
   `role` tinyint(4) NOT NULL DEFAULT '1',
-  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL DEFAULT 'uživatel',
+  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `first_login` int(11) DEFAULT NULL,
+  `last_login` int(11) DEFAULT NULL,
+  `banned` int(1) DEFAULT '0',
+  `active` int(1) DEFAULT '1',
+  `check_code` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `checked` int(1) DEFAULT '0',
+  `disabled` int(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -198,7 +182,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'email0','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',2,'hlavní admin'),(2,'email','$argon2i$v=19$m=1024,t=2,p=2$MmcvL3hJSUtvS1JyTHdWMw$7HtCPUhEcVLlgns6EJmqkUASf9S1K2GuRfEm6yMWgjE',2,'cvičitel'),(3,'account','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(4,'email2','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(5,'email3','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(6,'email4','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(7,'email5','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(8,'email6','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(9,'email7','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(10,'email8','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(11,'email9','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(12,'email10','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(13,'email11','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(14,'email12','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(15,'email13','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(16,'email14','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(17,'email15','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(18,'email16','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(19,'email17','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel'),(20,'email18','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel');
+INSERT INTO `users` VALUES (1,'admin@email.cz','$argon2i$v=19$m=1024,t=2,p=2$Z0t3TFdPNmlvNTJ3bW91WA$5EM9iHXwBaxCdv/k3x2nXsfUpRLGbUZ8CIlscSFunAM',2,'hlavní admin',NULL,1568904187,0,0,NULL,0,0),(2,'cvicitel@email.cz','$argon2i$v=19$m=1024,t=2,p=2$Z0t3TFdPNmlvNTJ3bW91WA$5EM9iHXwBaxCdv/k3x2nXsfUpRLGbUZ8CIlscSFunAM',2,'cvičitel',NULL,NULL,0,0,NULL,0,0),(3,'uzivatel@email.cz','$argon2i$v=19$m=1024,t=2,p=2$Z0t3TFdPNmlvNTJ3bW91WA$5EM9iHXwBaxCdv/k3x2nXsfUpRLGbUZ8CIlscSFunAM',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(4,'email2','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(5,'email3','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(6,'email4','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(7,'email5','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(8,'email6','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(9,'email7','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(10,'email8','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(11,'email9','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(12,'email10','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(13,'email11','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(14,'email12','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(15,'email13','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(16,'email14','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(17,'email15','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(18,'email16','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(19,'email17','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(20,'email18','$argon2i$v=19$m=1024,t=2,p=2$YkpmUDBWcnU1a05Kb0FtVw$xAdfBDwgksvTF0aCZ/VtTHOvSHALNXDkoCkcBAEn0oY',1,'uživatel',NULL,NULL,0,0,NULL,0,0),(21,'admin2@email.cz','$argon2i$v=19$m=1024,t=2,p=2$Z0t3TFdPNmlvNTJ3bW91WA$5EM9iHXwBaxCdv/k3x2nXsfUpRLGbUZ8CIlscSFunAM',1,'Petr Štechmüllerr',1568389453,1568904114,0,0,'8536163d3089db241affb6b91a8dd74af92d9ebf186ba71a6530e6c367aa45f6ce7d7fa0ebb4dfa350bf6e5c2fdc1f6353e2b705c82ac0563be7dc765c8c3f1b',0,0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -211,4 +195,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-10 19:01:40
+-- Dump completed on 2019-09-19 18:40:28
